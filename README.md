aeddbc4799452a960d9e480ffcff62061a10cd29
Rizwan Rizwan <rizwan.nazeerahmed@gmail.com>
2024-02-27 20:28:53 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+🦙💬 Code Llama Chatbot
+This chatbot is created using the open-source Code Llama model that has been tuned for code completion from Meta.
+
+Particularly, we're using the [codellama-7b-instruct](https://replicate.com/meta/codellama-7b-instruct) model hosted on the [Replicate platform](https://replicate.com/).
+
+
+## Prerequisite libraries
+streamlit
+replicate
+
+# Getting your own Replicate API token
+To use this app, you'll need to get your own Replicate API token.
+
+After signing up to Replicate, you can access your API token from this page.
+
+# Other Code Llama models to try
+You can also try out the implementation of Code Llama by [CharlyWargnier](https://github.com/CharlyWargnier):
+
+# Further Reading
+
+- [Code Llama blog](https://ai.meta.com/blog/code-llama-large-language-model-coding/)
+- [Code Llama research article](https://ai.meta.com/research/publications/code-llama-open-foundation-models-for-code/)
+- [Code Llama GitHub repo](https://github.com/facebookresearch/codellama)
+- [Download Code Llama](https://llama.meta.com/llama-downloads/)
---
+++ app.py
@@ -0,0 +1,86 @@
+import streamlit as st
+import replicate
+import os
+
+# App title
+st.set_page_config(page_title="🦙💬 Code Llama Chatbot")
+
+# Replicate Credentials
+with st.sidebar:
+    st.title('🦙💬 Code Llama Chatbot')
+    if 'REPLICATE_API_TOKEN' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    else:
+        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
+
+    st.subheader('Model parameters')
+    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
+    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+    top_k = st.sidebar.slider('top_k', min_value=0, max_value=512, value=250, step=1)
+    max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=128, step=8)
+    
+    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+os.environ['REPLICATE_API_TOKEN'] = replicate_api
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+# Function for generating LLaMA2 response
+# Refactored from https://github.com/a16z-infra/llama2-chatbot
+def generate_llama2_response(prompt_input):
+    
+    string_dialogue = """
+    You are a coding assistant.
+
+    You must follow the following rules strictly in generating your response:
+    1. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'. 
+    2. When generating the code answer, please encapsulate the code using ```
+    3. If you don't know, say you don't know and don't make up stuff.
+    4. Make your response concise, to the point and relevant to the question being asked.
+    """
+    
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    #output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
+    output = replicate.run('replicate/codellama-7b-instruct:0103579e86fc75ba0d65912890fa19ef03c84a68554635319accf2e0ba93d3ae',
+                        input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                "temperature":temperature, "top_p":top_p, "top_k":top_k, "max_length":max_length, "repetition_penalty":1.15})
+    return output
+
+# User-provided prompt
+if prompt := st.chat_input(disabled=not replicate_api):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_llama2_response(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
